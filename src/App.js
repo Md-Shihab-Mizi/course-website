c1c03341985e1eae0c7f22b76ea5caf94f5f7d38
Md-Shihab-Mizi <67514650+Md-Shihab-Mizi@users.noreply.github.com>
2020-08-27 01:26:05 +0600

    assingment start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fakeData from './fakeData/course';
+
+const data = fakeData.slice(0,15);
+console.log(data);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
   );
 }
---
+++ src/fakeData/course.js
@@ -0,0 +1,79 @@
+var course =[
+    {
+        id:1,
+        name:'Java',
+        price: 2999
+    },
+    {
+        id:2,
+        name:'Java Script',
+        price: 3000
+    },
+    {
+        id:3,
+        name:'React',
+        price: 2500
+    },
+    {
+        id:4,
+        name:'Boostrap',
+        price: 120
+    },
+    {
+        id:5,
+        name:'HTML',
+        price: 1500
+    },
+    {
+        id:6,
+        name:'CSS',
+        price: 999
+    },
+    {
+        id:7,
+        name:'Python',
+        price: 2250
+    },
+    {
+        id:8,
+        name:'Node JS',
+        price: 1920
+    },
+    {
+        id:9,
+        name:'Mongo DB',
+        price: 899
+    },
+    {
+        id:10,
+        name:'C++',
+        price: 3500
+    },
+    {
+        id:11,
+        name:'Mobile Photography',
+        price: 1500
+    },
+    {
+        id:12,
+        name:'Video Editing',
+        price: 2500
+    },
+    {
+        id:13,
+        name:'Adobe Photo Shop',
+        price: 3750
+    },
+    {
+        id:14,
+        name:'Word Press',
+        price: 600
+    },
+    {
+        id:15,
+        name:'Microsoft XL',
+        price: 700
+    },
+    
+]
+export default course;
